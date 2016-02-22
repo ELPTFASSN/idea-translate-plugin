9823f427dcbff41677648e0c9ad3b9ad69e94cbe
srcmaxim <srcmaxim@gmail.com>
2016-02-22 21:49:35 +0200

    Get selected text from IDEA

--- src/main/java/org/intellij/plugins/translate/TranslateAction.java
+++ src/main/java/org/intellij/plugins/translate/TranslateAction.java
@@ -2,11 +2,28 @@ package org.intellij.plugins.translate;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.DataKeys;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
 
-public class TranslateAction extends AnAction{
+import javax.swing.*;
+
+public class TranslateAction extends AnAction {
     @Override
-    public void actionPerformed(AnActionEvent e) {
+    public void actionPerformed(AnActionEvent event) {
 
+        Project project = event.getData(PlatformDataKeys.PROJECT);
+        Editor data = DataKeys.EDITOR.getData(event.getDataContext());
+
+        if (data != null) {
+            String selectedText = data.getSelectionModel().getSelectedText();
+            if (selectedText != null && selectedText.length() > 0) {
+
+                JOptionPane.showMessageDialog(null, selectedText,
+                        "Selected text", JOptionPane.PLAIN_MESSAGE);
+            }
+        }
     }
 }
 
