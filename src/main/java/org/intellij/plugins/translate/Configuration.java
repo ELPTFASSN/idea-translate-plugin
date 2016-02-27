793a9b90bf50e26b5200c4d0efa9c3819aa1d7c4
srcmaxim <srcmaxim@gmail.com>
2016-02-27 18:30:21 +0200

    Delete configuration. Strings from to are null

--- src/main/java/org/intellij/plugins/translate/Configuration.java
+++ src/main/java/org/intellij/plugins/translate/Configuration.java
@@ -6,7 +6,6 @@ import com.intellij.openapi.components.State;
 import com.intellij.openapi.components.Storage;
 import com.intellij.openapi.options.Configurable;
 import com.intellij.openapi.options.ConfigurationException;
-import com.intellij.util.xmlb.XmlSerializerUtil;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -21,16 +20,15 @@ public final class Configuration implements ProjectComponent,
         Configurable, PersistentStateComponent<Configuration> {
 
     public static final String COMPONENT_NAME = "Translate.Configuration";
-
+    public static final String NAME = "Yandex.Translator";
     public static final String CONFIGURATION_LOCATION;
 
     static {
         CONFIGURATION_LOCATION = System.getProperty("user.home");
     }
 
-    private ConfigurationForm form;
-    private String langFrom = Languages.ENGLISH;
-    private String langTo = Languages.RUSSIAN;
+    private String langFrom = null;
+    private String langTo = null;
 
     public String getFrom() {
         return langFrom;
@@ -47,18 +45,16 @@ public final class Configuration implements ProjectComponent,
 
     @Override
     public void initComponent() {
-
     }
 
     @Override
     public void disposeComponent() {
-
     }
 
     @Nls
     @Override
     public String getDisplayName() {
-        return "Yandex translate";
+        return NAME;
     }
 
     @Nullable
@@ -81,62 +77,36 @@ public final class Configuration implements ProjectComponent,
 
     @Override
     public void loadState(Configuration state) {
-        XmlSerializerUtil.copyBean(state, this);
     }
 
     @Override
     public void projectOpened() {
-
     }
 
     @Override
     public void projectClosed() {
-
     }
 
     @Nullable
     @Override
     public JComponent createComponent() {
-        if (form == null) {
-            form = new ConfigurationForm();
-        }
-
         return null;
-        //form.getRootComponent();
     }
 
     @Override
     public boolean isModified() {
-        return form != null /*&& form.isModified(this)*/;
+        return false;
     }
 
-    /**
-     * Stores settings from form to configuration bean.
-     *
-     * @throws ConfigurationException
-     */
     @Override
     public void apply() throws ConfigurationException {
-        if (form != null) {
-            //form.save(this);
-        }
     }
 
-    /**
-     * Restores form values from configuration.
-     */
     @Override
     public void reset() {
-        if (form != null) {
-            //form.load(this);
-        }
     }
 
-    /**
-     * Disposes UI resource.
-     */
     @Override
     public void disposeUIResources() {
-        form = null;
     }
 }
