1c20be5cac742a0f2abf9285d7db246e4ace5442
srcmaxim <srcmaxim@gmail.com>
2016-02-28 20:18:02 +0200

    Create tests for TranslationClient

--- src/test/java/org/intellij/plugins/translate/TranslationClientTest.java
+++ src/test/java/org/intellij/plugins/translate/TranslationClientTest.java
@@ -3,24 +3,30 @@ package org.intellij.plugins.translate;
 import org.junit.Assert;
 import org.junit.Test;
 
-import javax.swing.*;
 import java.io.IOException;
 import java.util.Set;
 
 public class TranslationClientTest {
+    public static final String FROM = "English";
+    public static final String TO = "Russian";
+
+    public static final String TEXT = "The quick brown fox jumps over the lazy dog";
+    public static final String TRANSLATED_TEXT = "Быстрая коричневая лиса прыгает через ленивую собаку";
 
     @Test
     public void testTranslate() throws TranslateException, IOException {
-        String langPair = Languages.transPairExist("English", "Russian");
-        String request = TranslationClient.translate("Hello!", langPair);
-        JOptionPane.showMessageDialog(null, request,
-                "Selected text", JOptionPane.PLAIN_MESSAGE);
-        Assert.assertNotNull(request);
+        String pair = Languages.transPairExist(FROM, TO);
+        String translatedText = TranslationClient.translate(TEXT, pair);
+
+        Assert.assertEquals(TRANSLATED_TEXT, translatedText);
     }
 
     @Test
     public void testGetLangPairs() throws IOException, TranslateException {
         Set<String> request = TranslationClient.getLangPairs();
         Assert.assertNotNull(request);
+        Assert.assertTrue(request.contains("en-ru"));
     }
+
+
 }
